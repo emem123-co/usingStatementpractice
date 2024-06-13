5d10bd4f5f07715557422be6c746280b0033d35e
Emily Morgan <emorgan921@gmail.com>
2024-06-13 16:11:15 -0400

    Add project files.

---
+++ usingStatementpractice/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace usingStatementpractice;
+
+internal class Program {
+    static void Main(string[] args) {
+
+        List<string> strings = new List<string>();
+
+        strings.Add("Pink");
+        strings.Add("Red");
+        strings.Add("Magenta");
+        strings.Add("Purple");
+        strings.Add("Black");
+        {
+
+            var answer = 0;
+
+            Console.Write("Type in a color ");
+            var color = Console.ReadLine();
+            if(strings.Contains(color)) {
+                Console.Write("Color already exisits!");
+            } else {
+                strings.Add(color);
+                Console.WriteLine("Added...");
+            }
+                foreach(string str in strings) {
+
+                    Console.Write($"{str} ");
+                }
+            }
+            
+        }
+    }
+
+
+
+
+
+
+
+
